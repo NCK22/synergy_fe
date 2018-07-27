3695958977f3c3db3655a33459f77358f15f4528
NCK22 <nehackulkarni22@gmail.com>
2018-07-27 18:28:31 +0530

    initial commit

---
+++ app/src/main/java/com/nyasa/synergyfieldengineer/Activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.nyasa.synergyfieldengineer.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nyasa.synergyfieldengineer.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/nyasa/synergyfieldengineer/VisitListActivity.java
@@ -0,0 +1,13 @@
+package com.nyasa.synergyfieldengineer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisitListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_list);
+    }
+}
