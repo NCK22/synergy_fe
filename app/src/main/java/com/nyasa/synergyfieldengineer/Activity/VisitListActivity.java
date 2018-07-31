850828f1102d19f24a7a5893c4b89dfeb5ff61fa
NCK22 <nehackulkarni22@gmail.com>
2018-07-31 18:15:05 +0530

    login design done

--- app/src/main/java/com/nyasa/synergyfieldengineer/Activity/VisitListActivity.java
+++ app/src/main/java/com/nyasa/synergyfieldengineer/Activity/VisitListActivity.java
@@ -1,7 +1,9 @@
-package com.nyasa.synergyfieldengineer;
+package com.nyasa.synergyfieldengineer.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nyasa.synergyfieldengineer.R;
 
 public class VisitListActivity extends AppCompatActivity {
 
